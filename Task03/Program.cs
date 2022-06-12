9ed046b41e735bb2cbb1a5e46f868a7df53323a3
Iryna_Akunevich <akunevichirina@mail.ru>
2022-06-12 15:26:16 +0300

    All eve numbers from 1 to N

---
+++ Task03/Program.cs
@@ -0,0 +1,20 @@
+﻿//адача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+
+using System;
+using static System.Console;
+Clear();
+
+WriteLine("Type any number:");
+int number = Convert.ToInt32(ReadLine());
+
+int currNumber = 2;
+
+if(number<=1) {WriteLine("Your number should be >1.");}
+else{
+    while (currNumber<=number)
+    {
+        Write(currNumber);
+        Write(", ");
+        currNumber = currNumber+2;
+    }
+}
