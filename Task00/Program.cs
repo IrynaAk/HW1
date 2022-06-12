9e09e43a4242f62db75978fc51a3e975ec8f43d2
Iryna_Akunevich <akunevichirina@mail.ru>
2022-06-12 13:47:56 +0300

    Search of a bigger number

---
+++ Task00/Program.cs
@@ -0,0 +1,20 @@
+﻿//Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+
+using System;
+using static System.Console;
+Clear();
+
+WriteLine("Type the first number:");
+int number1 = Convert.ToInt32(ReadLine());
+
+WriteLine("Type the second number:");
+int number2 = Convert.ToInt32(ReadLine());
+
+if (number1>number2)
+{
+    WriteLine("The first number is bigger than the second.");
+}
+else
+{
+    WriteLine("The second number is bigger than the first.");  
+}
